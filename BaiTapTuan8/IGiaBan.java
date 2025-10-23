79111672a08ad9c8fffb496c2be0306293a14025
Phan Thị Thu Thanh <thanhphanthu@gmail.com>
2025-10-23 14:05:10 +0700

    Delete BaiTapTuan8 directory

--- BaiTapTuan8/IGiaBan.java
+++
@@ -1,5 +0,0 @@
-
-package baitaptuan8;
-public interface IGiaBan {
-    double tinhGiaBan();
-}
--- BaiTapTuan8/IKiemKe.java
+++
@@ -1,6 +0,0 @@
-
-package baitaptuan8;
-public interface IKiemKe {
-    boolean kiemTraTonKho(int soLuongToiThieu);
-    void capNhatViTri(String viTriMoi);
-}
